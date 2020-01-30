32913675f4591ee088b2f1b504fc957aff93181e
hakida129 <vietlongit@gmail.com>
2020-01-30 11:48:26 +0800

    POST method

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const app = express();
+var bodyParser = require('body-parser');
 const port = 3000;
 
 app.set('views', './views');
 app.set('view engine', 'pug');
+
+app.use(express.json()); 
+app.use(express.urlencoded({ extended: true }));
 
 var users = [
     {id : 1, name :'long'},
@@ -15,13 +19,13 @@ app.get('/',function(req, res){
         name : 'hakida'
     });
 });
-app.get('/user',function(req, res){
+app.get('/users',function(req, res){
     res.render('users/index',{
         users: users
     });
 });
 
-app.get('/user/search',function(req,res){
+app.get('/users/search',function(req,res){
     var q = req.query.q;
     var matchedUsers = users.filter(function(user){
         return user.name.indexOf(q) !== -1;
@@ -30,6 +34,16 @@ app.get('/user/search',function(req,res){
         users: matchedUsers
     })
 });
+
+app.get('/users/create', function (req,res) {
+    res.render('users/create');
+  });
+
+app.post('/users/create', function (req,res) {  
+    users.push(req.body);
+    res.redirect('/users');
+});
+
 
 app.listen(port, function(){
     console.log(`Example app listerning on port ${port}`);
