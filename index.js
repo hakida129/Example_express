8c9e461f731e088cd601810fd593f80aed75ca62
hakida129 <vietlongit@gmail.com>
2020-01-30 10:36:28 +0800

    learn Query parameters

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ const port = 3000;
 app.set('views', './views');
 app.set('view engine', 'pug');
 
+var users = [
+    {id : 1, name :'long'},
+    {id : 2, name :'thang'}
+];
+
 app.get('/',function(req, res){
     res.render('index',{
         name : 'hakida'
@@ -12,11 +17,18 @@ app.get('/',function(req, res){
 });
 app.get('/user',function(req, res){
     res.render('users/index',{
-        users: [
-            {id : 1, name :'long'},
-            {id : 2, name :'thang'}
-        ]
+        users: users
     });
+});
+
+app.get('/user/search',function(req,res){
+    var q = req.query.q;
+    var matchedUsers = users.filter(function(user){
+        return user.name.indexOf(q) !== -1;
+    });
+    res.render('users/index',{
+        users: matchedUsers
+    })
 });
 
 app.listen(port, function(){
