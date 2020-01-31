623b1125cfd85a7113170f822522398c9d3bf0d0
hakida129 <vietlongit@gmail.com>
2020-01-31 20:34:49 +0800

    static file

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ const port = 3000;
 app.set('views', './views');
 app.set('view engine', 'pug');
 
+app.use(express.static('public'));
+
 app.use(express.json()); 
 app.use(express.urlencoded({ extended: true }));
 
