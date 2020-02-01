d15437440a69196ec701167919831caac651add7
hakida129 <vietlongit@gmail.com>
2020-02-01 22:21:35 +0800

    as

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -9,9 +9,9 @@ module.exports.index = function(req, res){
 };
 
 module.exports.search = function(req,res){
-    const q = req.query.q;
+    const name = req.query.name;
     const matchedUsers = db.get('users').value().filter(function(user){
-        return user.name.indexOf(q) !== -1;
+        return user.name.indexOf(name) !== -1;
     });
     res.render('users/index',{
         users: matchedUsers
@@ -33,6 +33,26 @@ module.exports.get = function(req, res){
 
 module.exports.postCreate = function (req,res) {  
     req.body.id = shortid.generate();
+
+    var errors = [];
+
+    if(!req.body.name){
+        errors.push('Name is required');
+    };
+
+    if(!req.body.phone){
+        errors.push('Phone is required');
+    };
+
+    if(errors.length){
+        res.render('users/create',{
+            errors: errors,
+            values: req.body
+        });
+        return;
+    }
+    
+
     db.get('users').push(req.body).write();
     res.redirect('/users');
 };
