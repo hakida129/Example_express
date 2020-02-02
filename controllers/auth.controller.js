7849f89e02710e2ace92de05bac105b585e9350d
hakida129 <vietlongit@gmail.com>
2020-02-02 23:16:56 +0800

    learn md5

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,3 +1,11 @@
+const md5 = require('md5');
+
+// node
+// > md5 = require('md5')
+// [Function]
+// > md5('123456')
+// 'e10adc3949ba59abbe56e057f20f883e'
+
 const db = require('../db');
 
 module.exports.login = function(req,res){
@@ -19,7 +27,10 @@ module.exports.postLogin = function(req, res){
         });
         return;
     };
-    if(user.password !== password){
+
+    const hashedPassword = md5(password);
+
+    if(user.password !== hashedPassword){
         res.render('auth/login',{
             errors: [
                 'Wrong password.'
