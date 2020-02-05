01c247259739382ee8d5d4ec146e3310ea0ad4d8
hakida129 <vietlongit@gmail.com>
2020-02-05 11:37:19 +0800

    upload file

--- routers/users.router.js
+++ routers/users.router.js
@@ -1,13 +1,16 @@
-const express = require('express')
+const express = require('express');
+var multer  = require('multer');
 
 const controller = require('../controllers/users.controller');
 const validate = require('../validate/user.validate');
+
+var upload = multer({ dest: './public/uploads/' })
 
 const router = express.Router()
 
 router.get('/', controller.index);
 
-router.get('/cookie', function(req, res, next){
+router.get('/cookie', function(req, res,  next){
     res.cookie('user-id', 12345);
     res.send('hello');
 });
@@ -18,6 +21,6 @@ router.get('/create', controller.create);
 
 router.get('/:id', controller.get);
 
-router.post('/create', validate.createPost, controller.postCreate);
+router.post('/create', upload.single('avatar'),  validate.createPost, controller.postCreate);
 
 module.exports = router;
